b7427394499e9e312f07bdaf48b8b2b80c0a08b8
2021000372PC <jhyou8912@gmail.com>
2021-11-26 17:40:31 +0900

    커스텀 유저 활장 v2

--- custom_users/admin.py
+++ custom_users/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from django.contrib.auth.models import Group
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import Users
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Users
+
+    # Admin User 화면에서 User 리스트에 출력되는 컬럼
+    list_display = ('email', 'full_name', 'gender', 'phone_number', 'user_grade', 'is_admin', 'is_active',)
+
+    # 화면 우측에 필터링 옵션에 출력되는 항목
+    list_filter = ('gender', 'user_grade', 'is_admin', 'is_active',)
+
+    fieldsets = (
+        ('필수정보', {'fields': ('email', 'password', 'full_name')}),
+        ('인적사항', {'fields': ('gender', 'phone_number')}),
+        ('권한설정', {'fields': ('user_grade', 'is_admin', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'full_name', 'is_admin', 'is_active')}
+         ),
+    )
+    #   add_fieldsets 의 내용을 아래와 같이 하면 회원가입시 필드 에러 발생
+    # add_fieldsets = (
+    #     ('필수정보', {'fields': ('email', 'password', 'full_name')}),
+    #     ('인적사항', {'fields': ('gender', 'phone_number')}),
+    #     ('권한설정', {'fields': ('user_grade', 'is_admin', 'is_active')}),
+    # )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(Users, CustomUserAdmin)
+admin.site.unregister(Group)
--- custom_users/models.py
+++ custom_users/models.py
@@ -1,19 +1,101 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
-class Users(AbstractUser):
-    userType = models.IntegerField(
-        default=4,
+# 유저 종류 설정
+GRADE_IN_ROLE_CHOICES = [
+    (1, '관리자'), (2, '대표'), (3, '강사'), (4, '강습회원')
+]
+
+GENDER_CHOICES = [
+    ('m', '남자'), ('f', '여자')
+]
+
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, full_name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            full_name=full_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, password):
+        user = self.create_user(
+            email,
+            password=password,
+            full_name=full_name,
+        )
+        # user.is_superuser = True
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    # Custom 헬퍼 클래스를 사용하도록 설정
+    objects = UserManager()
+
+    # Email - ID 역할
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=255,
+        unique=True,
+    )
+    # Username을 email로 명시
+    USERNAME_FIELD = 'email'
+
+    # 사이트 사용자 종류
+    #   1: 사이트 관리자
+    #   2: 업체 대표
+    #   3: 강사
+    #   4: 강습회원
+    user_grade = models.IntegerField(
+        choices=GRADE_IN_ROLE_CHOICES,
+        null=True, blank=True,
         validators=[
+            MinValueValidator(0),
             MaxValueValidator(4)
-        ]
+        ],
+        default=4
     )
 
-    userFullName = models.CharField(max_length=10)
+    full_name = models.CharField(max_length=20, null=False)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1, null=False)
 
-    userBirthdate = models.DateField
-    userSex = models.CharField(max_length=1)
+    date_of_birth = models.DateField
 
     phoneNumberRegex = RegexValidator(regex=r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
-    userPhone = models.CharField(validators=[phoneNumberRegex], max_length=11)
+    phone_number = models.CharField(validators=[phoneNumberRegex], max_length=11)
+
+    # is_staff: admin site 접근 가능 여부
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    # is_superuser = models.BooleanField(default=False)
+    # is_staff = models.BooleanField(default=False)
+
+    # REQUIRED_FIELDS 안 쓰면 createsuperuser 할 때 안 나타남
+    REQUIRED_FIELDS = ['full_name']
+
+    def __str__(self):
+        return self.email
+
+    # 커스텀 유저 모델을 기본 유저 모델로 사용하기 위해 구현한 부분
+    #   True를 반환하여 권한이 있음을 알림
+    def has_perm(self, perm, obj=None):
+        return True
+    #   True를 반환하여 주어진 App의 모델에 접근 가능하도록 함
+    def has_module_perms(self, app_label):
+        return True
+    #   True 가 반환되면 관리자 화면에 로그인 가능
+    @property
+    def is_staff(self):
+        return self.is_admin
